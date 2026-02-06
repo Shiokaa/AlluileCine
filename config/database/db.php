9290f81ab1854dc0980f5ca66953ca7751602422
Timothe <timothe.champieux@ynov.com>
2026-02-06 15:29:48 +0100

    feat(db): patern singleton creation

---
+++ config/database/db.php
@@ -0,0 +1,40 @@
+<?php
+
+class Database
+{
+    private static $_instance = null;
+    private $pdo;
+    private function __construct()
+    {
+        $host = 'localhost';
+        $dbName = 'AlluileCine';
+        $username = 'root';
+        $password = '';
+
+        $dsn = "mysql:host=$host;dbname=$dbName;charset=utf8mb4";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false, //Pour la sécurité, bloque mieux injections sql
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $username, $password, $options);
+        }
+        catch (PDOException $e) {
+            die("Erreur de connexion à la base de données : " . $e->getMessage());
+        }
+    }
+    public static function getInstance()
+    {
+        if (self::$_instance === null) {
+            self::$_instance = new Database();
+        }
+        return self::$_instance;
+    }
+    public function getConnection()
+    {
+        return $this->pdo;
+    }
+}
