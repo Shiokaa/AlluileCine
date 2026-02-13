148244918e5a00410acb2c7bdcfc6b40daf63023
Shiokaa <tom.amaru09@gmail.com>
2026-02-13 01:50:20 +0100

    feat(home): remove mock movie data and integrate dynamic movie retrieval

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,47 +1,5 @@
 <?php include_once __DIR__ . "/partials/header.php" ?>
 
-<?php
-// Données factices pour l'affichage (à remplacer plus tard par la base de données)
-$movies = [
-    [
-        'title' => 'Inception',
-        'genre' => 'Science-fiction',
-        'duration' => '2h 28min',
-        'image' => 'https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg'
-    ],
-    [
-        'title' => 'The Dark Knight',
-        'genre' => 'Action, Crime',
-        'duration' => '2h 32min',
-        'image' => 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg'
-    ],
-    [
-        'title' => 'Interstellar',
-        'genre' => 'Aventure, Drame',
-        'duration' => '2h 49min',
-        'image' => 'https://image.tmdb.org/t/p/w500/gEU2QniL6E8ahMcafHC7347wAr4.jpg'
-    ],
-    [
-        'title' => 'Avatar : La Voie de l\'eau',
-        'genre' => 'Science-fiction, Action',
-        'duration' => '3h 12min',
-        'image' => 'https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg'
-    ],
-    [
-        'title' => 'Top Gun: Maverick',
-        'genre' => 'Action, Drame',
-        'duration' => '2h 11min',
-        'image' => 'https://image.tmdb.org/t/p/w500/62HCnUTziyWcpDaBO2i1DX17ljH.jpg'
-    ],
-     [
-        'title' => 'Dune',
-        'genre' => 'Science-fiction',
-        'duration' => '2h 35min',
-        'image' => 'https://image.tmdb.org/t/p/w500/d5NXSklXo0qyLXUdbVRvdEVnOTq.jpg'
-    ]
-];
-?>
-
 <div class="hero">
     <h1>Bienvenue sur AlluileCiné</h1>
     <p>Réservez vos places pour les meilleurs films du moment en quelques clics.</p>
@@ -51,23 +9,27 @@ $movies = [
     <h2 class="section-title">À l'affiche actuellement</h2>
     
     <div class="movies-grid">
+        <?php if ($response['status']): ?>
         <?php foreach($movies as $movie): ?>
             <div class="movie-card">
-                <img src="<?= $movie['image'] ?>" alt="<?= $movie['title'] ?>" class="movie-poster">
+                <img src="<?= "/assets/img/movies/". $movie['cover_image'] ?>" alt="<?= $movie['title'] ?>" class="movie-poster">
                 <div class="movie-info">
                     <h3 class="movie-title"><?= $movie['title'] ?></h3>
                     <div class="movie-genre"><?= $movie['genre'] ?></div>
                     <div class="movie-duration">
-                        <i class="fa-regular fa-clock"></i> <?= $movie['duration'] ?>
+                        <i class="fa-regular fa-clock"></i> <?= floor($movie['duration'] / 60) ?>h <?= str_pad($movie['duration'] % 60, 2, '0', STR_PAD_LEFT) ?>min
                     </div>
                     <div class="movie-actions">
-                        <a href="#" class="btn-book">
+                        <a href="/movies/<?= $movie['id'] ?>" class="btn-book">
                             <i class="fa-solid fa-ticket"></i> Réserver
                         </a>
                     </div>
                 </div>
             </div>
         <?php endforeach; ?>
+        <?php else: ?>
+                <p><?= $response['message'] ?></p>
+        <?php endif; ?>
     </div>
 </div>
 
